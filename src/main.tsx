ae6205c33075efa0646e51208fe32976a18f243e
Sergey Chama <sergeychama24@gmail.com>
2025-04-23 21:34:12 +0300

    feat: add scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './app/App.tsx'
+import "./styles/index.scss";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./app/App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-
-)
+);
