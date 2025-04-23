a39408184d24b4f74285a8a2d0ea341086c18874
Sergey Chama <sergeychama24@gmail.com>
2025-04-24 02:45:14 +0300

    fix: fix Header component

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -9,18 +9,22 @@ export function Header() {
 
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>DARK STORE</div>
-      <ul className={styles.links}>
-        <li>Еда</li>
-        <li>Электроника</li>
-        <li>Одежда</li>
-      </ul>
-      <div className={styles.cart}>
-        <Cart isOpen={isOpenCart} />
-        <CiShoppingCart
-          size={"2rem"}
-          onClick={() => setIsOpenCart((s) => !s)}
-        />
+      <div className={styles.container}>
+        <nav className={styles.nav}>
+          <div className={styles.logo}>DARK STORE</div>
+          <ul className={styles.links}>
+            <li>Еда</li>
+            <li>Электроника</li>
+            <li>Одежда</li>
+          </ul>
+        </nav>
+        <div>
+          <Cart isOpen={isOpenCart} />
+          <CiShoppingCart
+            className={styles.cartIcon}
+            onClick={() => setIsOpenCart((s) => !s)}
+          />
+        </div>
       </div>
     </header>
   );
